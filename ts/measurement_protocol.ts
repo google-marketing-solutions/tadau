d45bbb9799e0bb0b1f92a97df46916079d8d8113
Maximilian Weiss <maximilianw@google.com>
2024-06-17 14:35:40 -0700

    No public description
    PiperOrigin-RevId: 644136849

---
+++ ts/measurement_protocol.ts
@@ -0,0 +1,334 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Tadau is a library to send events to Google Analytics 4
+ * events using Measurement Protocol.
+ *
+ * Expected usage:
+ *
+ * // Using API secret and measurement ID.
+ * const tadau = new Tadau({
+ *   apiSecret: 'my-secret',
+ *   measurementId: 'G-123456789',
+ *   fixedDimensions: {
+ *     'dimension1': 'value1',
+ *     'dimension2': 'value2',
+ *   },
+ *   optIn: true,
+ * });
+ *
+ * // Using a config file.
+ * const tadau = new Tadau({
+ *   configFilePath: 'path/to/config.yaml',
+ *   optIn: true,
+ * });
+ *
+ * // Using dedicated methods.
+ * tadau.sendAdsEvent(
+ *   'click', 'my-campaign', 'google-ads', '123456789', 'ad', '123456789'
+ * );
+ * tadau.sendCustomEvent('click', true, 'my-campaign');
+ * tadau.sendErrorEvent('error-message', '404', 'my-campaign', '123456789');
+ *
+ * // Using events array.
+ * tadau.sendEvents([
+ *   {
+ *     'name': 'ads-event',
+ *     'event_is_impact_action': true,
+ *     'event_action': 'click',
+ *     'event_context': 'my-campaign',
+ *     'ads_platform': 'google-ads',
+ *     'ads_platform_id': '123456789',
+ *     'ads_resource': 'ad',
+ *     'ads_resource_id': '123456789',
+ *   },
+ *   {
+ *     'name': 'custom-event',
+ *     'event_action': 'click',
+ *     'event_is_impact_action': true,
+ *     'event_context': 'my-campaign',
+ *   },
+ *   {
+ *     'name': 'error-event',
+ *     'error_message': 'error-message',
+ *     'error_code': '404',
+ *     'error_location': 'my-campaign',
+ *     'error_location_id': '123456789',
+ *   },
+ * ]);
+ */
+
+const GA_COLLECT_URL = 'https://www.google-analytics.com/mp/collect';
+const GA_RESERVED_KEYS = ['app_instance_id', 'uuid', 'timestamp_micros'];
+
+/**
+ * Checks if a parameter is valid.
+ * @param key The parameter key.
+ * @param value The parameter value.
+ * @param reservedKeys The list of reserved keys.
+ * @return True if the parameter is valid.
+ */
+function isValidParam(
+  key: string,
+  value: string | number | boolean,
+  reservedKeys: string[],
+): boolean {
+  return !reservedKeys.includes(key) && value !== null && value !== undefined;
+}
+
+/**
+ * Formats a string to alphanumeric.
+ * @param text The text to format.
+ * @return The formatted text.
+ */
+function formatToAlphanumeric(text: string): string {
+  return text.replace(/[^a-zA-Z0-9-]/g, ''); // Use regex replacement directly
+}
+
+/**
+ * Tadau configuration.
+ */
+interface TadauConfig {
+  apiSecret?: string;
+  measurementId?: string;
+  fixedDimensions?: {[key: string]: string | number | boolean};
+}
+
+/**
+ * Tadau parameters.
+ */
+interface TadauParams {
+  apiSecret?: string;
+  measurementId?: string;
+  fixedDimensions?: {[key: string]: string | number | boolean};
+  configFilePath?: string; // Path to a YAML file with the configuration
+  optIn?: boolean;
+}
+
+/**
+ * This class is used to send events to Google Analytics 4 using Measurement
+ * Protocol.
+ */
+export class Tadau {
+  /**
+   * @param apiUrl The API URL.
+   * @param reservedKeys The reserved keys.
+   * @param apiSecret The API secret.
+   * @param measurementId The measurement ID.
+   * @param fixedDimensions The fixed dimensions.
+   * @param optIn The opt-in.
+   * @param targetUrl The target URL.
+   */
+  private readonly apiUrl = GA_COLLECT_URL;
+  private readonly reservedKeys = GA_RESERVED_KEYS;
+  private readonly targetUrl?: string;
+
+  apiSecret?: string;
+  measurementId?: string;
+  fixedDimensions?: {[key: string]: string | number | boolean};
+  optIn?: boolean;
+
+  constructor(params: TadauParams) {
+    const {apiSecret, measurementId, fixedDimensions, configFilePath, optIn} =
+      params;
+
+    if (configFilePath) {
+      const config = this.loadConfigFromYamlFile(configFilePath);
+      this.apiSecret = config?.apiSecret || apiSecret;
+      this.measurementId = config?.measurementId || measurementId;
+      this.fixedDimensions = config?.fixedDimensions || fixedDimensions || {};
+    } else {
+      this.apiSecret = apiSecret;
+      this.measurementId = measurementId;
+      this.fixedDimensions = fixedDimensions || {};
+    }
+
+    if (this.apiSecret && this.measurementId) {
+      this.targetUrl = `${this.apiUrl}?api_secret=${this.apiSecret}&measurement_id=${this.measurementId}`;
+    }
+
+    this.optIn = optIn || false;
+
+    if (!this.isValidInstance()) {
+      throw new Error(
+        'Tadau: Class could not be initiated because api_secret and/or measurement_id is invalid',
+      );
+    }
+  }
+
+  /**
+   * Checks if the instance is valid.
+   * @return True if the instance is valid.
+   */
+  private isValidInstance(): boolean {
+    return !!this.apiSecret && !!this.measurementId;
+  }
+
+  /**
+   * Loads the configuration from a YAML file.
+   * @param configFilePath The path to the YAML file.
+   * @return The configuration.
+   */
+  // tslint:disable-next-line:no-any
+  private loadConfigFromYamlFile(configFilePath: string): TadauConfig | void {
+    try {
+      // TODO(): Figure out how to use the config file. Apps Script
+      // does not support reading yaml files, nor having any other files than
+      // .gs or .html files.
+      console.info('Tadau: Loading from config file is not implemented yet.');
+      return;
+    } catch (error) {
+      console.error('Tadau: Error loading configuration:', error);
+    }
+  }
+
+  /**
+   * Sends events to Google Analytics 4.
+   * @param events The events to send.
+   */
+  sendEvents(events: Array<{[key: string]: string | number | boolean}>): void {
+    if (!events || events.length === 0) {
+      console.debug('Tadau: events empty.');
+      return;
+    }
+    if (!this.optIn) {
+      console.debug('Tadau: Opt-in is false.');
+      return;
+    }
+
+    for (const row of events) {
+      try {
+        const clientId = row['client_id'] || Utilities.getUuid();
+        const payload: {[key: string]: string | number | boolean | object} = {
+          'non_personalized_ads': true,
+          'client_id': clientId,
+        };
+
+        const eventReservedKeys = [...this.reservedKeys, 'name'];
+        const params = {...this.fixedDimensions};
+        for (const [k, v] of Object.entries(row)) {
+          if (isValidParam(k, v, eventReservedKeys)) {
+            params[k] = v;
+          } else {
+            console.warn(`Tadau: Parameter ${k} with value ${v} is not valid`);
+          }
+        }
+
+        payload['events'] = [
+          {
+            'name': formatToAlphanumeric(row['name'] as string),
+            params,
+          },
+        ];
+
+        const userId = row['user_id'];
+        if (userId) {
+          payload['user_id'] = userId;
+        }
+
+        const response = UrlFetchApp.fetch(this.targetUrl!, {
+          method: 'post',
+          payload: JSON.stringify(payload),
+        });
+        if (response.getResponseCode() !== 204) {
+          throw new Error(`Error sending event: ${response.getContentText()}`);
+        }
+
+        console.info('Tadau: Successfully sent event:', payload);
+      } catch (error) {
+        console.error(`Tadau: Error sending event to GA4 property: ${error}`);
+      }
+    }
+  }
+
+  /**
+   * Sends an Ads event to Google Analytics 4.
+   * @param eventAction The event action.
+   * @param eventContext The event context.
+   * @param adsPlatform The Ads platform.
+   * @param adsPlatformId The Ads platform ID.
+   * @param adsResource The Ads resource.
+   * @param adsResourceId The Ads resource ID.
+   */
+  sendAdsEvent(
+    eventAction: string,
+    eventContext: string,
+    adsPlatform: string,
+    adsPlatformId: string,
+    adsResource: string,
+    adsResourceId: string,
+  ): void {
+    this.sendEvents([
+      {
+        'name': 'ads-event',
+        'event_is_impact_action': true,
+        'event_action': eventAction,
+        'event_context': eventContext,
+        'ads_platform': adsPlatform,
+        'ads_platform_id': adsPlatformId,
+        'ads_resource': adsResource,
+        'ads_resource_id': adsResourceId,
+      },
+    ]);
+  }
+
+  /**
+   * Sends a custom event to Google Analytics 4.
+   * @param eventAction The event action.
+   * @param eventIsImpactAction The event is impact action.
+   * @param eventContext The event context.
+   */
+  sendCustomEvent(
+    eventAction: string,
+    eventIsImpactAction: boolean,
+    eventContext: string,
+  ): void {
+    this.sendEvents([
+      {
+        'name': 'custom-event',
+        'event_action': eventAction,
+        'event_is_impact_action': eventIsImpactAction,
+        'event_context': eventContext,
+      },
+    ]);
+  }
+
+  /**
+   * Sends an error event to Google Analytics 4.
+   * @param errorMessage The error message.
+   * @param errorCode The error code.
+   * @param errorLocation The error location.
+   * @param errorLocationId The error location ID.
+   */
+  sendErrorEvent(
+    errorMessage: string,
+    errorCode: string,
+    errorLocation: string,
+    errorLocationId: string,
+  ): void {
+    this.sendEvents([
+      {
+        'name': 'error-event',
+        'error_message': errorMessage,
+        'error_code': errorCode,
+        'error_location': errorLocation,
+        'error_location_id': errorLocationId,
+      },
+    ]);
+  }
+}
---
+++ ts/measurement_protocol_test.ts
@@ -0,0 +1,264 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {setUpAppsScriptSimulator} from 'google3/javascript/apps/maestro/simulator/closure_apps_script_simulator-closurized';
+import 'jasmine';
+import {Tadau} from './measurement_protocol';
+
+describe('Tadau', () => {
+  let tadau: Tadau;
+  let urlFetchAppSpy: jasmine.Spy;
+
+  beforeEach(() => {
+    setUpAppsScriptSimulator();
+    tadau = new Tadau({
+      apiSecret: 'test_api_secret',
+      measurementId: 'test_measurement_id',
+      optIn: true,
+    });
+    spyOn(Utilities, 'getUuid').and.returnValue('test_client_id');
+    urlFetchAppSpy = spyOn(UrlFetchApp, 'fetch').and.returnValue(
+      createFakeResponse(200),
+    );
+  });
+  function createFakeResponse(
+    responseCode: number,
+  ): GoogleAppsScript.URL_Fetch.HTTPResponse {
+    return {
+      getResponseCode: () => responseCode,
+    } as GoogleAppsScript.URL_Fetch.HTTPResponse;
+  }
+
+  it('should be instantiated', () => {
+    expect(tadau).toBeDefined();
+  });
+
+  it('should send events', () => {
+    const events = [
+      {
+        'name': 'test-event',
+        'test_param': 'test_value',
+      },
+    ];
+
+    tadau.sendEvents(events);
+
+    expect(urlFetchAppSpy).toHaveBeenCalledWith(
+      'https://www.google-analytics.com/mp/collect?api_secret=test_api_secret&measurement_id=test_measurement_id',
+      {
+        method: 'post',
+        payload: JSON.stringify({
+          'non_personalized_ads': true,
+          'client_id': 'test_client_id',
+          'events': [
+            {
+              'name': 'test-event',
+              'params': {
+                'test_param': 'test_value',
+              },
+            },
+          ],
+        }),
+      },
+    );
+  });
+
+  it('should send ads event', () => {
+    tadau.sendAdsEvent(
+      'test_action',
+      'test_context',
+      'test_platform',
+      'test_platform_id',
+      'test_resource',
+      'test_resource_id',
+    );
+
+    expect(urlFetchAppSpy).toHaveBeenCalledWith(
+      'https://www.google-analytics.com/mp/collect?api_secret=test_api_secret&measurement_id=test_measurement_id',
+      {
+        method: 'post',
+        payload: JSON.stringify({
+          'non_personalized_ads': true,
+          'client_id': 'test_client_id',
+          'events': [
+            {
+              'name': 'ads-event',
+              'params': {
+                'event_is_impact_action': true,
+                'event_action': 'test_action',
+                'event_context': 'test_context',
+                'ads_platform': 'test_platform',
+                'ads_platform_id': 'test_platform_id',
+                'ads_resource': 'test_resource',
+                'ads_resource_id': 'test_resource_id',
+              },
+            },
+          ],
+        }),
+      },
+    );
+  });
+
+  it('should send custom event', () => {
+    tadau.sendCustomEvent('test_action', true, 'test_context');
+
+    expect(urlFetchAppSpy).toHaveBeenCalledWith(
+      'https://www.google-analytics.com/mp/collect?api_secret=test_api_secret&measurement_id=test_measurement_id',
+      {
+        method: 'post',
+        payload: JSON.stringify({
+          'non_personalized_ads': true,
+          'client_id': 'test_client_id',
+          'events': [
+            {
+              'name': 'custom-event',
+              'params': {
+                'event_action': 'test_action',
+                'event_is_impact_action': true,
+                'event_context': 'test_context',
+              },
+            },
+          ],
+        }),
+      },
+    );
+  });
+
+  it('should send error event', () => {
+    tadau.sendErrorEvent(
+      'test_error_message',
+      'test_error_code',
+      'test_error_location',
+      'test_error_location_id',
+    );
+
+    expect(urlFetchAppSpy).toHaveBeenCalledWith(
+      'https://www.google-analytics.com/mp/collect?api_secret=test_api_secret&measurement_id=test_measurement_id',
+      {
+        method: 'post',
+        payload: JSON.stringify({
+          'non_personalized_ads': true,
+          'client_id': 'test_client_id',
+          'events': [
+            {
+              'name': 'error-event',
+              'params': {
+                'error_message': 'test_error_message',
+                'error_code': 'test_error_code',
+                'error_location': 'test_error_location',
+                'error_location_id': 'test_error_location_id',
+              },
+            },
+          ],
+        }),
+      },
+    );
+  });
+
+  it('should not send events when not opt-in.', () => {
+    const events = [
+      {
+        'name': 'test-event',
+        'test_param': 'test_value',
+      },
+    ];
+    tadau = new Tadau({
+      apiSecret: 'test_api_secret',
+      measurementId: 'test_measurement_id',
+      optIn: false,
+    });
+    tadau.sendEvents(events);
+
+    expect(urlFetchAppSpy).not.toHaveBeenCalled();
+  });
+
+  it('should throw error if api secret or measurement id is invalid', () => {
+    expect(() => {
+      tadau = new Tadau({
+        apiSecret: '',
+        measurementId: 'test_measurement_id',
+      });
+    }).toThrowError(
+      'Tadau: Class could not be initiated because api_secret and/or measurement_id is invalid',
+    );
+
+    expect(() => {
+      tadau = new Tadau({
+        apiSecret: 'test_api_secret',
+        measurementId: '',
+      });
+    }).toThrowError(
+      'Tadau: Class could not be initiated because api_secret and/or measurement_id is invalid',
+    );
+  });
+
+  it('should not send events if events is empty', () => {
+    tadau.sendEvents([]);
+
+    expect(urlFetchAppSpy).not.toHaveBeenCalled();
+  });
+
+  it('should not send events if event is invalid', () => {
+    tadau.sendEvents([
+      {
+        'app_instance_id': 'reserverd-key',
+      },
+    ]);
+
+    expect(urlFetchAppSpy).not.toHaveBeenCalled();
+  });
+
+  it('should send events with fixed dimensions', () => {
+    tadau = new Tadau({
+      apiSecret: 'test_api_secret',
+      measurementId: 'test_measurement_id',
+      fixedDimensions: {
+        'test_fixed_dimension': 'test_fixed_value',
+      },
+      optIn: true,
+    });
+
+    const events = [
+      {
+        'name': 'test-event',
+        'test_param': 'test_value',
+      },
+    ];
+
+    tadau.sendEvents(events);
+
+    expect(urlFetchAppSpy).toHaveBeenCalledWith(
+      'https://www.google-analytics.com/mp/collect?api_secret=test_api_secret&measurement_id=test_measurement_id',
+      {
+        method: 'post',
+        payload: JSON.stringify({
+          'non_personalized_ads': true,
+          'client_id': 'test_client_id',
+          'events': [
+            {
+              'name': 'test-event',
+              'params': {
+                'test_fixed_dimension': 'test_fixed_value',
+                'test_param': 'test_value',
+              },
+            },
+          ],
+        }),
+      },
+    );
+  });
+});
